65bc458c8d6f73564d81121e77c043e75074915f
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-03-20 11:24:10 +0100

    create : AppDbContext

---
+++ API/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+    }
+}
