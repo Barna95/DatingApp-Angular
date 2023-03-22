2be8affa4f5e88ed038086142b7e1b37aa68e115
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-03-22 14:11:45 +0100

    create : ServiceExtensions
    Identity and App extensions for cleaner code

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,24 @@
+﻿using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddScoped<ITokenService, TokenService>();
+
+            services.AddDbContext<AppDbContext>(options => options
+                .UseSqlServer(config
+                    .GetConnectionString("DefaultConnectionString")));
+
+            services.AddCors();
+
+            return services;
+        }
+    }
+}
