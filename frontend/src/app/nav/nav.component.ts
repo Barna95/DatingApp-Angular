2caa474a9dc581ef62e31e913e0d2dbcb2123311
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-03-31 13:24:48 +0200

    chore : components
    remove not necessary code parts

--- frontend/src/app/nav/nav.component.ts
+++ frontend/src/app/nav/nav.component.ts
@@ -11,7 +11,7 @@ import { ToastrService } from 'ngx-toastr';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) { }
+  constructor(public accountService: AccountService, private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -19,7 +19,7 @@ export class NavComponent implements OnInit {
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: () =>  this.router.navigateByUrl('/members'),
+      next: () => this.router.navigateByUrl('/members')
       })
   }
 
