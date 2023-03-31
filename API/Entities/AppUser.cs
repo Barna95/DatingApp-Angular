6a0acc2224f7b146dba2d3f06905b81746c78a25
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-03-31 13:26:06 +0200

    update : extended AppUsers

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,10 +1,28 @@
-﻿namespace API.Entities
+﻿using API.Extensions;
+using Microsoft.AspNetCore.Identity;
+
+namespace API.Entities
 {
     public class AppUser
     {
         public int Id { get; set; }
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }    
+        public byte[] PasswordSalt { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+        public DateTime LastActive { get; set; } = DateTime.UtcNow;
+        public string Gender { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string Country { get; set; }
+        public List<Photo> Photos { get; set; } = new ();
+
+        public int GetAge()
+        {
+            return DateOfBirth.CalculateAge();
+        }
     }
 }
