026defc6f31e964129c23a0a386fe42c6a4113b2
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-03-31 13:27:22 +0200

    create : DateTimeExtensions

---
+++ API/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace API.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static int CalculateAge(this DateTime dob)
+        {
+            //dob = Date Of Birth
+            var today = DateTime.UtcNow;
+            var age = today.Year - dob.Year;
+
+            if (dob > today) age= int.Abs(age);
+            // no leap years counted yet so its not perfect 
+            return age;
+        }
+    }
+}
