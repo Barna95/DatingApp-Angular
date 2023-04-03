25d7e75d60e418324a9678eab7db50eeb77fe525
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-04-03 15:43:53 +0200

    update : ApplicationServiceExtensions
    added AutoMapper, and user repository

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -11,6 +11,8 @@ namespace API.Extensions
         {
             services.AddEndpointsApiExplorer();
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
             services.AddDbContext<AppDbContext>(options => options
                 .UseSqlServer(config
