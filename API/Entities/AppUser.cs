29ac135d514823015b127714e3997d8a918d9a55
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-04-04 11:58:31 +0200

    update : memberDto and AppUser

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,5 +1,4 @@
 ﻿using API.Extensions;
-using Microsoft.AspNetCore.Identity;
 
 namespace API.Entities
 {
@@ -18,6 +17,7 @@ namespace API.Entities
         public string LookingFor { get; set; }
         public string Interests { get; set; }
         public string Country { get; set; }
+        public string City { get; set; }
         public List<Photo> Photos { get; set; } = new ();
 
         public int GetAge()
