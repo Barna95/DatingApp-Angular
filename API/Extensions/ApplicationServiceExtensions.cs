df69cce40649bd9383489c7b61c4d57c490a5c65
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-04-13 10:00:20 +0200

    update: ApplicationServiceExtension
    configure Cloudinary

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using API.Data;
+using API.Helpers;
 using API.Interfaces;
 using API.Services;
 using Microsoft.EntityFrameworkCore;
@@ -13,6 +14,7 @@ namespace API.Extensions
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
 
             services.AddDbContext<AppDbContext>(options => options
                 .UseSqlServer(config
