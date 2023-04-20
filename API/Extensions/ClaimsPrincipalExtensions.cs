28db2a99c38f1821737e170c8320b8aeb35df2f5
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-04-20 17:04:20 +0200

    update: ClaimsPrincipalExtension
    GetUserId method added

--- API/Extensions/ClaimsPrincipalExtensions.cs
+++ API/Extensions/ClaimsPrincipalExtensions.cs
@@ -7,6 +7,12 @@ namespace API.Extensions
         public static string GetUsername(this ClaimsPrincipal user)
         {
             //to recognize the user we have to use ClaimTypes.NameIdentifier, not the JwtRegisteredClaimNames.NameId part even after registering with that
+            return user.FindFirst(ClaimTypes.Name)?.Value;
+        }
+
+        public static string GetUserId(this ClaimsPrincipal user)
+        {
+            //to recognize the user we have to use ClaimTypes.NameIdentifier, not the JwtRegisteredClaimNames.NameId part even after registering with that
             return user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
         }
     }
