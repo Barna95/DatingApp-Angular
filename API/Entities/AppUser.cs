506745ee42e9e67c7ecae3c1faf58c1b37711321
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-04-27 11:54:47 +0200

    update: AppUser
    message props

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -24,5 +24,7 @@ namespace API.Entities
         //public List<AppUser> LikedUsers { get; set; } the table names will end up weird too
         public List<UserLike> LikedByUsers { get; set; }
         public List<UserLike> LikedUsers { get; set; }
+        public List<Message> MessagesSent { get; set; }
+        public List<Message> MessagesReceived { get; set; }
     }
 }
