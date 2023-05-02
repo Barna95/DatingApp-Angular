7a24320d15c761c3c391ac9e1137dfbd81043a88
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-05-02 14:38:05 +0200

    create: member-messages component

---
+++ frontend/src/app/members/member-messages/member-messages.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Message } from '../../_models/message';
+import { MessageService } from '../../_services/message.service';
+
+@Component({
+  selector: 'app-member-messages',
+  templateUrl: './member-messages.component.html',
+  styleUrls: ['./member-messages.component.css']
+})
+export class MemberMessagesComponent implements OnInit {
+  @Input() username?: string;
+  @Input() messages: Message[] = [];
+
+  constructor(private messageService: MessageService) { }
+
+  ngOnInit(): void {
+  }
+
+}
