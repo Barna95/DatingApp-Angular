7abbda83685bde2aa6d4dcfb9686652ac91dafd6
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-05-04 14:35:18 +0200

    update: MessageRepository
    check for who deleted the message, and show messages depending on the deletions

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -39,9 +39,12 @@ namespace API.Data
 
             query = messageParams.Container switch
             {
-                "Inbox" => query.Where(u => u.RecipientUsername == messageParams.Username),
-                "Outbox" => query.Where(u => u.SenderUsername == messageParams.Username),
-                _ => query.Where(u => u.RecipientUsername == messageParams.Username && u.DateRead == null)
+                "Inbox" => query.Where(u => u.RecipientUsername == messageParams.Username &&
+                                            u.RecipientDeleted == false),
+                "Outbox" => query.Where(u => u.SenderUsername == messageParams.Username &&
+                                             u.SenderDeleted == false),
+                _ => query.Where(u => u.RecipientUsername == messageParams.Username &&
+                                      u.DateRead == null && u.RecipientDeleted == false)
             };
 
             var messages = query.ProjectTo<MessageDto>(_mapper.ConfigurationProvider);
@@ -55,9 +58,9 @@ namespace API.Data
                 .Include(u => u.Sender).ThenInclude(p => p.Photos)
                 .Include(u => u.Recipient).ThenInclude(p => p.Photos)
                 .Where(
-                    m => m.RecipientUsername == currentUserName &&
+                    m => m.RecipientUsername == currentUserName && m.RecipientDeleted == false &&
                          m.SenderUsername == recipientUserName ||
-                         m.RecipientUsername == recipientUserName &&
+                         m.RecipientUsername == recipientUserName && m.SenderDeleted == false &&
                          m.SenderUsername == currentUserName
                 )
                 .OrderBy(m => m.MessageSent)
