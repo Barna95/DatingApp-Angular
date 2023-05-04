452a2acaeb0f142fbd0277012a14f3ba870a73af
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-05-04 10:18:34 +0200

    update: member-messages component

--- frontend/src/app/members/member-messages/member-messages.component.ts
+++ frontend/src/app/members/member-messages/member-messages.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Message } from '../../_models/message';
 import { MessageService } from '../../_services/message.service';
 
@@ -10,10 +11,22 @@ import { MessageService } from '../../_services/message.service';
 export class MemberMessagesComponent implements OnInit {
   @Input() username?: string;
   @Input() messages: Message[] = [];
+  messageContent = '';
+  @ViewChild('messageForm') messageForm?: NgForm
 
   constructor(private messageService: MessageService) { }
 
   ngOnInit(): void {
+  }
+
+  sendMessage() {
+    if (!this.username) return;
+    this.messageService.sendMessage(this.username, this.messageContent).subscribe({
+      next: message => {
+        this.messages.push(message);
+        this.messageForm?.reset();
+      }
+      })
   }
 
 }
