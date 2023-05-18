8aae61900bb8ccfd7200081d974ced8c1e95d321
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-05-18 10:01:25 +0200

    create: PresenceHub
    for connected users with SignalR

---
+++ API/SignalR/PresenceHub.cs
@@ -0,0 +1,28 @@
+﻿using API.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace API.SignalR
+{
+    public class PresenceHub : Hub
+    {
+        //signalR uses websockets protocol instead of http protocol
+        [Authorize]
+        public override async Task OnConnectedAsync()
+        {
+            //clients object from SignalR allows us to use methods on our clients
+            //Specify to who we send the message example: user, other, etc.
+            //name of the method that should run
+            //Context is the Hub-s context and gives us access to our user claims principal so we can use their methods too
+            //everyone will get this information that the user that logged in is online here
+            await Clients.Others.SendAsync("UserIsOnline", Context.User.GetUsername());
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Clients.Others.SendAsync("UserIsOffline", Context.User.GetUsername());
+
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
