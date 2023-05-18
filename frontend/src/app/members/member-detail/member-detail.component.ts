96416f737b54c9b5d55e47bb88845de5b902eee6
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-05-18 16:12:14 +0200

    update: member-detail
    added hub connection, and the needed services to work with SignalR

--- frontend/src/app/members/member-detail/member-detail.component.ts
+++ frontend/src/app/members/member-detail/member-detail.component.ts
@@ -1,29 +1,44 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions } from '@kolkov/ngx-gallery';
 import { TabDirective, TabsetComponent } from 'ngx-bootstrap/tabs';
 import { ToastrService } from 'ngx-toastr';
+import { take } from 'rxjs';
 import { Member } from '../../_models/member';
 import { Message } from '../../_models/message';
+import { User } from '../../_models/user';
+import { AccountService } from '../../_services/account.service';
 import { MembersService } from '../../_services/members.service';
 import { MessageService } from '../../_services/message.service';
+import { PresenseService } from '../../_services/presense.service';
 
 @Component({
   selector: 'app-member-detail',
   templateUrl: './member-detail.component.html',
   styleUrls: ['./member-detail.component.css']
 })
-export class MemberDetailComponent implements OnInit {
+export class MemberDetailComponent implements OnInit, OnDestroy {
   @ViewChild('memberTabs', {static: true}) memberTabs?: TabsetComponent;
   member: Member = {} as Member;
   galleryOptions: NgxGalleryOptions[] = [];
   galleryImages: NgxGalleryImage[] = [];
   activeTab?: TabDirective;
   messages: Message[] = [];
+  user?: User;
 
   constructor(private memberService: MembersService, private route: ActivatedRoute,
-    private messageService: MessageService, private toastr: ToastrService) { }
-
+    private messageService: MessageService, private toastr: ToastrService,
+    public presenceService: PresenseService, private accountService: AccountService,
+    private router: Router)
+  {
+    this.accountService.currentUser$.pipe(take(1)).subscribe({
+      next: user => {
+        if (user) this.user = user;
+      }
+    });
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+  }
+ 
   ngOnInit(): void {
     this.route.data.subscribe({
       next: data => this.member = data['member']
@@ -42,6 +57,10 @@ export class MemberDetailComponent implements OnInit {
       preview: false,
     }]
     this.galleryImages = this.getImages();
+  }
+
+  ngOnDestroy(): void {
+    this.messageService.stopHubConnection();
   }
 
   getImages() {
@@ -78,8 +97,10 @@ export class MemberDetailComponent implements OnInit {
 
   onTabActivated(data: TabDirective) {
     this.activeTab = data;
-    if (this.activeTab.heading === 'Messages') {
-      this.loadMessages();
+    if (this.activeTab.heading === 'Messages' && this.user) {
+      this.messageService.createHubConnection(this.user, this.member.userName);
+    } else {
+      this.messageService.stopHubConnection();
     }
   }
 }
