7fe03e2786b17797070d1223ac080471e8e9cd73
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-05-18 16:05:30 +0200

    create: connection and group entity

---
+++ API/Entities/Connection.cs
@@ -0,0 +1,20 @@
+﻿namespace API.Entities
+{
+    public class Connection
+    {
+        public string ConnectionId { get; set; }
+        public string Username { get; set; }
+
+        public Connection(string connectionId, string username)
+        {
+            ConnectionId = connectionId;
+            Username = username;
+        }
+
+        // to satisfy EF when we create the DB, we need to give this an empty constructor, since we can't expect the connection infos at start
+        public Connection()
+        {
+            
+        }
+    }
+}
