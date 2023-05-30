a4bda30f532c187b6582f37eaf1720ea26b097f5
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-05-30 12:49:20 +0200

    create: UnitOfWork
    Add unit of work

---
+++ API/Data/UnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using API.Interfaces;
+using AutoMapper;
+
+namespace API.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
+
+        public UnitOfWork(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public IUserRepository UserRepository => new UserRepository(_context, _mapper);
+        public IMessageRepository MessageRepository => new MessageRepository(_context, _mapper);
+        public ILikesRepository LikesRepository => new LikesRepository(_context);
+        public async Task<bool> Complete()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public bool HasChanges()
+        {
+            //Changetracker.haschanges is giving back a bool, just sparing myself from some additional coding
+            return _context.ChangeTracker.HasChanges();
+        }
+    }
+}
