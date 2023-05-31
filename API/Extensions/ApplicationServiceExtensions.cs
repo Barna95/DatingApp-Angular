411a6b13180fa2c07908916ba6e2911ac30180c7
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-05-31 17:35:08 +0200

    update: ApplicationServiceExtensions
    using Npgsql instead of SqlServer

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -22,7 +22,7 @@ namespace API.Extensions
             services.AddScoped<IUnitOfWork, UnitOfWork>();
 
             services.AddDbContext<AppDbContext>(options => options
-                .UseSqlServer(config
+                .UseNpgsql(config
                     .GetConnectionString("DefaultConnectionString")));
 
             services.AddCors();
