33699dcb1ac1c0ef7e8dcbfa8cddd160b56c1c02
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-06-01 11:18:41 +0200

    update: program.cs
    added logic to get the connection string if we are on build environment

--- API/Program.cs
+++ API/Program.cs
@@ -2,6 +2,7 @@ using API.Data;
 using API.Extensions;
 using API.Middleware;
 using API.SignalR;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 namespace API
@@ -22,6 +23,34 @@ namespace API
                 .WriteTo.File("D:\\DateApp\\log.txt").MinimumLevel.Warning()
                 .WriteTo.File("D:\\DateApp\\structuredLog.json").MinimumLevel.Warning());
 
+            var connString = "";
+
+            if (builder.Environment.IsDevelopment())
+                connString = builder.Configuration.GetConnectionString("DefaultConnection");
+            else
+            {
+                // Use connection string provided at runtime by FlyIo.
+                var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+                // Parse connection URL to connection string for Npgsql
+                connUrl = connUrl.Replace("postgres://", string.Empty);
+                var pgUserPass = connUrl.Split("@")[0];
+                var pgHostPortDb = connUrl.Split("@")[1];
+                var pgHostPort = pgHostPortDb.Split("/")[0];
+                var pgDb = pgHostPortDb.Split("/")[1];
+                var pgUser = pgUserPass.Split(":")[0];
+                var pgPass = pgUserPass.Split(":")[1];
+                var pgHost = pgHostPort.Split(":")[0];
+                var pgPort = pgHostPort.Split(":")[1];
+                var updatedHost = pgHost.Replace("flycast", "internal");
+
+                connString = $"Server={updatedHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+                }
+                   builder.Services.AddDbContext<AppDbContext>(opt =>
+                {
+                   opt.UseNpgsql(connString);
+                });
+
 
             var app = builder.Build();
 
@@ -41,10 +70,14 @@ namespace API
 
             app.UseAuthorization();
 
+            app.UseDefaultFiles(); //look for index.html in wwwroot and serve by default
+            app.UseStaticFiles(); //look for wwwroot and serve the content from inside there by default
+
             app.MapControllers();
             //create endpoint for SignalR, and which class it uses
             app.MapHub<PresenceHub>("hubs/presence");
             app.MapHub<MessageHub>("hubs/message");
+            app.MapFallbackToController("Index", "Fallback");
 
             Seed.SeedUsers(app);
 
